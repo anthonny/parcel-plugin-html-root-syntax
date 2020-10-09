f12a8e372d072fe131a1d32d848f41ca79553471
Anthonny <anthonny.querouil@gmail.com>
2020-10-09 23:44:31 +0200

    add support of icon

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
-const fs = require('fs')
-const cheerio = require('cheerio')
+const fs = require("fs");
+const cheerio = require("cheerio");
 
 const tags = {
-  script: 'src',
-  'link[rel="stylesheet"]': 'href',
-}
+  script: "src",
+  'link[rel="stylesheet"]': "href",
+  'link[rel="icon"]': "href",
+  'link[rel="apple-touch-icon"]': "href",
+};
 
 function replaceRootSyntaxWithAbsolutePath(bundle) {
-  if (bundle.type === 'html') {
-    const filePath = bundle.name
-    const content = fs.readFileSync(filePath, 'utf8')
-    const $ = cheerio.load(content)
+  if (bundle.type === "html") {
+    const filePath = bundle.name;
+    const content = fs.readFileSync(filePath, "utf8");
+    const $ = cheerio.load(content);
 
-    let shouldUpdate = false
+    let shouldUpdate = false;
 
     Object.entries(tags).forEach(([tag, attribute]) => {
       $(tag).each((i, el) => {
-        const $el = $(el)
-        const prop = $el.attr(attribute)
+        const $el = $(el);
+        const prop = $el.attr(attribute);
 
-        if (prop && prop.startsWith('#')) {
-          $el.attr(attribute, prop.replace(/^#/, ''))
-          shouldUpdate = true
+        if (prop && prop.startsWith("#")) {
+          $el.attr(attribute, prop.replace(/^#/, ""));
+          shouldUpdate = true;
         }
-      })
+      });
 
       if (shouldUpdate) {
-        fs.writeFileSync(filePath, $.html())
+        fs.writeFileSync(filePath, $.html());
       }
-    })
+    });
   }
 }
 
 function forEachBundle(fn, bundle) {
-  fn(bundle)
+  fn(bundle);
 
   if (bundle.childBundles && bundle.childBundles.size) {
-    for (let childBundle of bundle.childBundles) { // eslint-disable-line no-restricted-syntax
-      fn(childBundle)
+    for (let childBundle of bundle.childBundles) {
+      // eslint-disable-line no-restricted-syntax
+      fn(childBundle);
     }
   }
 }
 
 module.exports = function (bundler) {
-  bundler.on('bundled', (bundle) => {
-    forEachBundle(replaceRootSyntaxWithAbsolutePath, bundle)
-  })
-}
+  bundler.on("bundled", (bundle) => {
+    forEachBundle(replaceRootSyntaxWithAbsolutePath, bundle);
+  });
+};
